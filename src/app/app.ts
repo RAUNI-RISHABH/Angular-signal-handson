c3acf989a8207e30256604bc5967d9fcb70bc39b
RAUNI-RISHABH <rishabh.prakash42@gmail.com>
2025-10-19 23:34:23 +0530

    cahgnes done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
 export class App {
   protected readonly title = signal('angular-signal');
 
-  quantity = signal(0);
+  quantity = signal(1);
+  qtyAvailable = signal([1, 2, 3, 4, 5, 6]);
+  selectedVe
+constructor() {
+  console.log("quantity calue", this.quantity());
+  // to update quantity
+  this.quantity.update(qty => qty * 2);
+}
+
+qtyEff = effect(() => console.log("latest qty: ", this.quantity()))
+
+onQuantitySelected(qty: number) {
+  this.quantity.set(qty);
+  // this.quantity.set(5);
+  // this.quantity.set(42);
+}
+
 }
